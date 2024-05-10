ea82ed85f0227019b57c4bfdb4572a9cd62c964b
muradorucov <orucowmurad@gmail.com>
2024-05-10 12:56:55 +0400

    folder structer done

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import "./assets/reset.css";
+import "./assets/layout.css";
+import "./assets/style.css";
+import "./assets/responsive.css";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
